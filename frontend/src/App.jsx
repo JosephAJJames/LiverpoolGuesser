55195996c2d955acec17e5a643a5208e89c1176c
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-07-13 14:01:03 +0100

    Added a homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Homepage from './homepage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <p>Hello, World</p>
+      <Homepage/>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ frontend/src/homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Homepage() {
+    return (
+        <>
+            <h1>Liverpool Player Guesser</h1>
+            <p>How high can your score go?</p>
+            <button>start</button>
+        </>
+    );
+};
+
+export default Homepage;
