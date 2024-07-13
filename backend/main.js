192ba5c5194b62c57ffc57461d1cdc6c665e0f58
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-07-13 13:33:09 +0100

    setup

--- backend/main.js
+++ backend/main.js
@@ -3,6 +3,10 @@ const db_setup = require("./db_setup");
 
 const app = express();
 
+app.get("/api", (req, res) => {
+    console.log("api pinged");
+});
+
 app.listen(8000, () => {
     db_setup();
 });
